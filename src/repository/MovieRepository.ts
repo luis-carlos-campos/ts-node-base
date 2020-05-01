17d2a9d3a9d3029788142eebf5b7b3c109b44a73
Luís Carlos Campos <luis_campos@apple.com>
2020-04-30 22:49:11 -0300

    Removed repository layer that was not being used

--- src/repository/MovieRepository.ts
+++
@@ -1,7 +0,0 @@
-import { EntityRepository, Repository } from "typeorm";
-import Project from "../entity/Project";
-
-@EntityRepository(Project)
-export class ProjectRepository extends Repository<Project> {}
-
-export default ProjectRepository;
